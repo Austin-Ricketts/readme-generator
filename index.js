940dd2133259af7d46cd2ee81d33fd82fc16170c
Austin Ricketts <austinricketts4@gmail.com>
2020-04-24 22:56:27 -0400

    emphasized

---
+++ index.js
@@ -0,0 +1,14 @@
+const generateMarkdown = require("./node_modules/util/generateMarkdown.js");
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
